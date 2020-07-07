0a954fa3dee80e437b789a986cf220ca7c3e0bf9
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-07 20:48:59 +0200

    Initial commit. Closes #1

---
+++ src/main/java/guru/springframework/sbmssm/SbmSsmApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sbmssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbmSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sbmssm/SbmSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sbmssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
