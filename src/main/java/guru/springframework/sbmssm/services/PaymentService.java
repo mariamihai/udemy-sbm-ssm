ee5c76524236081f3c44a838793fce90859fdd92
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-07 22:59:07 +0200

    Payment Service Creation. Closes #7

---
+++ src/main/java/guru/springframework/sbmssm/services/PaymentService.java
@@ -0,0 +1,17 @@
+package guru.springframework.sbmssm.services;
+
+import guru.springframework.sbmssm.domain.Payment;
+import guru.springframework.sbmssm.domain.PaymentEvent;
+import guru.springframework.sbmssm.domain.PaymentState;
+import org.springframework.statemachine.StateMachine;
+
+public interface PaymentService {
+
+    Payment newPayment(Payment payment);
+
+    StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId);
+
+    StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
+}
